c1a53b3864583798897061d30dc034311f662602
J Resilient <j.resilient.1@gmail.com>
2021-07-31 18:00:03 -0400

    Add pseudocode for backend of game

---
+++ src/board.js
@@ -0,0 +1,8 @@
+class Board {
+    constructor() {
+        // create and store a Snake
+        // later we'll store apples
+    }
+}
+
+// export Board for the view
---
+++ src/snake.js
@@ -0,0 +1,24 @@
+class Snake {
+    // store snake directions - up, down, right, left
+    constructor() {
+        // store snake segments (grid coordinates - presumably where each part of the snake is at the current moment)
+        // starting position???
+        // store the current direction
+    }
+
+    move() {
+        // move the snake in the current direction
+    }
+
+    turn(dir) {
+        // change the current direction
+    }
+
+    _plus(c1, c2) {
+        // sum and return the coordinates
+    }
+
+    _equals(c1, c2) {
+        // check if the coordinates are equal??
+    }
+}
