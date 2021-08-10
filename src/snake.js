6c0d23f6b0e8c28bbe2051df895ecd1ea43c73d5
J Resilient <j.resilient.1@gmail.com>
2021-08-09 20:54:42 -0400

    Add turn() and move() to Snake class

--- src/snake.js
+++ src/snake.js
@@ -1,24 +1,45 @@
 class Snake {
-    // store snake directions - up, down, right, left
+    
     constructor() {
-        // store snake segments (grid coordinates - presumably where each part of the snake is at the current moment)
-        // starting position???
-        // store the current direction
-    }
+        // I'm picturing a 400x400 grid for the moment
+        this.tail = [[200, 200]];
+        this.currentDir = "U";
 
+        // up, down, right, left
+        this.dirs = { "U": [0, -1], "D": [0, 1], "R": [1, 0], "L": [-1, 0] };
+    }
+    
     move() {
         // move the snake in the current direction
+        for (let i = 0; i < this.tail.length; i++) {
+            this.tail[i] = Snake.plus(this.tail[i], this.dirs[this.currentDir]);
+        }
     }
 
     turn(dir) {
         // change the current direction
+        this.currentDir = dir;
     }
 
-    _plus(c1, c2) {
+    addSegment() {
+        // add a new segment to the end of the snake
+    }
+
+    // static?
+    static plus(c1, c2) {
         // sum and return the coordinates
+        return [(c1[0] + c2[0]), (c1[1] + c2[1])];
     }
 
-    _equals(c1, c2) {
-        // check if the coordinates are equal??
+    // static?
+    static equals(c1, c2) {
+        // check if the coordinates are equal
+    }
+
+    // static?
+    static isOpposite(c1, c2) {
+        // checks if the second position is opposite the first
     }
 }
+
+let snake = new Snake();
