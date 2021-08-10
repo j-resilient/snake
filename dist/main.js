8cfdcb5079d676cf8d45813dc27f93c77f37ba1d
J Resilient <j.resilient.1@gmail.com>
2021-08-09 21:19:03 -0400

    Code in main.js is generated by webpack

--- dist/main.js
+++ dist/main.js
@@ -13,20 +13,54 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("console.log(\"webpack is working\");\n\n//# sourceURL=webpack://snake/./src/index.js?");
+eval("console.log(\"webpack is working\");\nconst SnakeView = __webpack_require__(/*! ./snake-view */ \"./src/snake-view.js\");\n\n$(() => {\n    const $el = $(\".snake\");\n    new SnakeView($el);\n})\n\n//# sourceURL=webpack://snake/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/snake-view.js":
+/*!***************************!*\
+  !*** ./src/snake-view.js ***!
+  \***************************/
+/***/ ((module) => {
+
+eval("// require board\nclass View {\n    constructor($el) {\n        this.$el = $el;\n        // build a board\n        // bind a listener to detect key events, use jquery on with \"keydown\"\n        // use setInterval: call step() every half-second\n    }\n\n    handleKeyEvent(e) {\n        // look up e.keyCode\n        // turn snake\n    }\n\n    step() {\n        // call Snake#move\n        // redraw board - render\n    }\n\n    render() {\n        // render board\n    }\n}\n\nmodule.exports = View;\n\n//# sourceURL=webpack://snake/./src/snake-view.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
