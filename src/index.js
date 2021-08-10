cac107e4036b95ddb6020b1830af08d245911feb
J Resilient <j.resilient.1@gmail.com>
2021-08-09 21:08:53 -0400

    Add pseudocode in comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log("webpack is working");
+// require view
---
+++ src/snake-view.js
@@ -0,0 +1,25 @@
+// require board
+class View {
+    constructor($el) {
+        // store html container element
+        // build a board
+        // bind a listener to detect key events, use jquery on with "keydown"
+        // use setInterval: call step() every half-second
+    }
+
+    handleKeyEvent(e) {
+        // look up e.keyCode
+        // turn snake
+    }
+
+    step() {
+        // call Snake#move
+        // redraw board - render
+    }
+
+    render() {
+        // render board
+    }
+}
+
+// export view class
