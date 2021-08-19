c1b2a73824086d1904acaca9c042072eadff6cea
J Resilient <j.resilient.1@gmail.com>
2021-08-18 20:34:49 -0400

    Move grid creation to its own method in snake-view

--- src/snake-view.js
+++ src/snake-view.js
@@ -4,9 +4,20 @@ class View {
     constructor($el) {
         this.$el = $el;
         this.board = new Board();
-        this.render();
+        this._createGrid();
         // bind a listener to detect key events, use jquery on with "keydown"
         // use setInterval: call step() every half-second
+    }
+
+    _createGrid() {
+        for (let row = 0; row < 20; row++) {
+            for (let col = 0; col < 20; col++) {
+                let $square = $('<div></div>');
+                $square.addClass('box');
+                $square.data("pos", [col, row]);
+                this.$el.append($square);
+            }
+        }
     }
 
     handleKeyEvent(e) {
@@ -20,22 +31,7 @@ class View {
     }
 
     render() {
-        // render board
-        // let $square = $('<div></div>');
-        // $square.addClass('green');
-        // $square.addClass('box');
-        // this.$el.append($square);
-
-        for (let row = 0; row < 20; row++) {
-            for (let col = 0; col < 20; col++) {
-                let $square = $('<div></div>');
-                $square.addClass('box');
-                if (col === 9 && row === 9) {
-                    $square.addClass('green');
-                }
-                this.$el.append($square);
-            }
-        }
+        
     }
 }
 
