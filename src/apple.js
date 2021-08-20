753f3dfcd703bbc34c06f26626bd5bbb0fa39e9d
J Resilient <j.resilient.1@gmail.com>
2021-08-19 21:59:41 -0400

    Add pseudocode in Apple

---
+++ src/apple.js
@@ -0,0 +1,15 @@
+class Apple {
+    constructor() {
+        // pos = random position
+    }
+
+    randomPos() {
+        // generate random position
+    }
+
+    newApple() {
+        // generate a new location
+    }
+}
+
+module.exports = Apple;
