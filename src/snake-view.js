d767765676e8b40601001b6494c3a01e7cd7db43
J Resilient <j.resilient.1@gmail.com>
2021-08-19 20:16:00 -0400

    Got render() in snake-view working

--- src/snake-view.js
+++ src/snake-view.js
@@ -5,6 +5,14 @@ class View {
         this.$el = $el;
         this.board = new Board();
         this._createGrid();
+
+        // for (let i = 0; i < 3; i++) {
+        //     setTimeout(() => {
+        //         this.board.snake.move();
+        //         this.render();
+        //     }, 5000);
+        //     // this.board.snake.turn("R");
+        // }
         // bind a listener to detect key events, use jquery on with "keydown"
         // use setInterval: call step() every half-second
     }
@@ -15,6 +23,8 @@ class View {
                 let $square = $('<div></div>');
                 $square.addClass('box');
                 $square.data("pos", [col, row]);
+                // the snake starts at the center of the grid
+                if (col === 9 && row === 9) { $square.addClass('green'); }
                 this.$el.append($square);
             }
         }
@@ -31,7 +41,16 @@ class View {
     }
 
     render() {
-        
+        const $boxes = $("div.box");
+        for (let i = 0; i < $boxes.length; i++) {
+            let $currentBox = $($boxes[i]);
+            let boxPos = $currentBox.data("pos");
+            if (this.board.snake.tail.some(pos => pos[0] === boxPos[0] && pos[1] === boxPos[1])) {
+                $currentBox.addClass('green');
+            } else {
+                $currentBox.removeClass('green');
+            }
+        }
     }
 }
 
